3c2a51a03c0d02003489d78db804c988de899963
janiapurva <jani.apurva@yahoo.com>
2021-01-14 17:56:40 -0500

    adding files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,33 @@
 const request = require('request');
-let input = process.argv.slice(2).toString();
-//console.log (input)
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // Print the body.
-  const data = JSON.parse(body);
-  //console.log(data[0].name);
-  
-  if (data[0]) {
-    console.log(data[0].description);
-  } else {
-    console.log('Breed not found');
-  }
 
-});
+const fetchBreedDescription  = function(breedName,callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // Print the body.
+    
+    
+    if (error) {
+      callback(error,null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null,data[0].description);
+    } else {
+      let nobreed = 'Breed Not Found';
+      callback(nobreed);
+    }
+  
+
+  
+    // if (data[0]) {
+    //   return (data[0].description);
+    // } else {
+    //   return 'Breed not found';
+    // }
+
+  });
+};
+
+module.exports = {fetchBreedDescription};
